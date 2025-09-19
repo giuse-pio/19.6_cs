e041d6a186cbd1fa827ebb409d676f82151252f3
giupe <giupe@MSI>
2025-09-19 18:09:33 +0200

    Add project files.

---
+++ 19.6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _19._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> eta = new List<int>();
+            int n;
+            Console.WriteLine("inserisci l'età di una persona");
+            while (int.TryParse(Console.ReadLine(), out n) || n <= 0)
+            {
+                if (n <= 0)
+                {
+                    break;
+                }
+                eta.Add(n);
+            }
+            
+            Console.WriteLine();
+            for (int i = 0; i < eta.Count; i++)
+            {
+                int pos = i + 1;
+                if (eta[i] > 18)
+                {
+                    Console.WriteLine($"La persona in posizione {pos} ha {eta[i]}");
+                }
+            }
+        }
+    }
+}
